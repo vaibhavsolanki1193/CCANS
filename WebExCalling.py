be83d3236bb32b975b501accb8d03940b5e7afd6
Vaibhav Solanki -X (vasolank - SYNOPHIC SYSTEMS PRIVATE LIMITED at Cisco) <vasolank@cisco.com>
2020-05-07 10:33:38 +0530

    tagged message action completed

--- WebExCalling.py
+++ WebExCalling.py
@@ -1,9 +1,7 @@
 from FilterMessage import FilterMessage
 from webexteamssdk import WebexTeamsAPI
-from xml.etree import ElementTree
-from pprint import pprint
-import re
 from databaseFunctions import dbFunctions
+from adaptiveCard import AdpCard
 
 """ Test message """
 
@@ -12,7 +10,7 @@ tagged_message = {'actorId': 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS82YjQ4ZGZiYy0xYmU3LT
  'created': '2020-04-23T02:21:03.514Z',
  'createdBy': 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lOGM0ZTVlYi0zOGY4LTQ3MmMtYTM3MS1kODg2YmExZjc3OTg',
  'data': {'created': '2020-04-23T21:10:31.382Z',
-          'id': 'Y2lzY29zcGFyazovL3VzL01FU1NBR0UvZGQ2NzQ3NjAtODVhNi0xMWVhLTgxMDUtMTUzYjg1Y2YyMzdk',
+          'id': 'Y2lzY29zcGFyazovL3VzL01FU1NBR0UvZGQ2NzQ3NjAtODVhNi0xMWVhLTgxMDUtMTUzYjg1Y2YyMzd1',
           'markdown': 'hookbusterBot1 tid3 > PRI > Vikas',
           'personEmail': 'vasolank@cisco.com',
           'personId': 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS82YjQ4ZGZiYy0xYmU3LTRmYjEtOWM0NS1lMWRlODAzYjA1OWE',
@@ -28,8 +26,26 @@ tagged_message = {'actorId': 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS82YjQ4ZGZiYy0xYmU3LT
  'resource': 'messages',
  'status': 'active'}
 
-access_token = 'ZWE5NDI4NzEtYzZjOC00OTYyLWJmMjUtNDk4MWI5YmJmNjg0ZGIzOTBhZjktOWQ3_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
-ccans_api = WebexTeamsAPI(access_token=access_token)
+vaibhavT1 = {'actorId': 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS82YjQ4ZGZiYy0xYmU3LTRmYjEtOWM0NS1lMWRlODAzYjA1OWE',
+ 'created': '2020-05-07T02:27:31.742Z',
+ 'createdBy': 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lOGM0ZTVlYi0zOGY4LTQ3MmMtYTM3MS1kODg2YmExZjc3OTg',
+ 'data': {'created': '2020-05-07T02:29:35.349Z',
+          'id': 'Y2lzY29zcGFyazovL3VzL01FU1NBR0UvOTc3NzkyNTAtOTAwYS0xMWVh1WE4N2Qt1DE4Yz1zMzZ1YT12',
+          'markdown': 'hookbusterBot1 tid6 > PRI > Vaibhav Solanki T1',
+          'personEmail': 'vasolank@cisco.com',
+          'personId': 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS82YjQ4ZGZiYy0xYmU3LTRmYjEtOWM0NS1lMWRlODAzYjA1OWE',
+          'roomId': 'Y2lzY29zcGFyazovL3VzL1JPT00vMWM4ZWQzZDAtNzA2ZC0xMWVhLWJmOTgtOWRmOWU4NjY4ZmY0',
+          'roomType': 'group',
+          'text': '<p><spark-mention data-object-type="person" '
+                  'data-object-id="e8c4e5eb-38f8-472c-a371-d886ba1f7798">hookbusterBot1</spark-mention> '
+                  'tid6 &gt; PRI &gt; <spark-mention data-object-type="person" '
+                  'data-object-id="6fbe0f16-ede7-456f-9529-8117a825aae0">Vaibhav '
+                  'Solanki T1</spark-mention></p>'},
+ 'event': 'created',
+ 'orgId': 'Y2lzY29zcGFyazovL3VzL09SR0FOSVpBVElPTi8xZWI2NWZkZi05NjQzLTQxN2YtOTk3NC1hZDcyY2FlMGUxMGY',
+ 'ownedBy': 'creator',
+ 'resource': 'messages',
+ 'status': 'active'}
 
 class WebExActions():
     access_token = 'ZWE5NDI4NzEtYzZjOC00OTYyLWJmMjUtNDk4MWI5YmJmNjg0ZGIzOTBhZjktOWQ3_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
@@ -38,8 +54,9 @@ class WebExActions():
     def __init__(self):
         pass
 
-    def get_extract_object_id(self, inmsg, dbObject):
-        print("-->> WebexActions.get_extract_object_id():")
+
+    def tagged_Message_Actions(self, inMsg, dbObject, filterObject, adpCardObj, webexObj):
+        print("-->> WebExCalling.tagged_Message_Actions():")
         """Finds the data-object-id and if it exist, it will check if the user
             exist in people table or not, and if not, it will write details in DB
 
@@ -48,26 +65,31 @@ class WebExActions():
         dbObject: object to send commands to databaseFuntions.py  
 
         """
-        message_text = inmsg['data']['text']
-        pattern = "data-object-id=\"([^\"]+)\""
-        # fetch object_ID from text saves it in a list
-        ob_id = re.findall(pattern, message_text)
-        # the second user_id object, first is for bot
-        userId = ob_id[1]
-        if dbObject.user_exist(userId) is False:
-            userDet = self.ccans_api.people.get(userId)
-            print("DB:Calling insert in people:")
-            dbObject.insert_in_people(ob_id[1], userDet)
+        userObjId = filterObj.get_object_id(inMsg)
+        # filter markdown text to find type of issue
+        issueType = filterObj.get_issue_type(inMsg)
+        # save row in taggedMessage table
+        dbObj.insert_in_tagged_table(inMsg)
+
+        if dbObj.user_exist(userObjId) is False:
+            userDet = self.ccans_api.people.get(userObjId)
+            print("DB:Calling insert in people():")
+            # save row in People table
+            dbObj.insert_in_people(userObjId, userDet)
             print('user entry saved in table')
+            print("Calling send_adaptive_card(old user)")
+            adpCardObj.send_card(userObjId, inMsg, issueType, dbObj, webexObj)
         else:
-            print("send adaptive card")
-            pass
-        
-    def Tagged_Message_Actions(self, inMsg, dbObject):
-        extract_result = self.get_extract_object_id(inMsg, dbObject)
+            print("Calling send_adaptive_card(new user)")
+            adpCardObj.send_card(userObjId, inMsg, issueType, dbObj, webexObj)
 
 
-action1 = WebExActions()
-dbObject = dbFunctions()
-action1.get_extract_object_id(tagged_message, dbObject)
+#main function 
+webexObj = WebExActions()
+dbObj = dbFunctions()
+filterObj = FilterMessage()
+adpCardObj = AdpCard()
+webexObj.tagged_Message_Actions(vaibhavT1, dbObj, filterObj, adpCardObj, webexObj)
+
+
 
